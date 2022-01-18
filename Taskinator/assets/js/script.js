b5694bb05a789d97cbe11f8b4c2a0dadbe7ab3ec
MercuryKTgit <kimdotnetpossible@att.net>
2022-01-18 16:39:57 -0600

    minimum script for addinga task with task info using form-submit

--- Taskinator/assets/js/script.js
+++ Taskinator/assets/js/script.js
@@ -1,11 +1,35 @@
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var formEl = document.querySelector("#task-form");
+
+// unorderred list obj waiting for listItemEl has a div child item call taskInfoEl
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
+
+// Dynamically get the value of the input 
+// var taskNameInput = document.querySelector("input[name='task-name']").value;
+//var taskNameInputVice = document.getElementById("input[name='tast-name']");
+//console.log(taskNameInput);
+
+var createTaskHandler = function(event) {
+  event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  // give it a class name
+  taskInfoEl.className = "task-info";
+
+  // add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild(taskInfoEl); // add div element to li element, div element is consider a child of li in this case
+
+  //listItemEl.textContent = taskNameInput; //"This is a new task.";
+  tasksToDoEl.appendChild(listItemEl); //ul adds li
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
+
+//buttonEl.addEventListener("click", createTaskHandler);
