232e07cdce45628655eb2b431f8b30194414d16b
MercuryKTgit <kimdotnetpossible@att.net>
2022-01-19 00:26:02 -0600

    add task-editing capacity but not yet perfect - only renter the current data for editing

--- Taskinator/assets/js/script.js
+++ Taskinator/assets/js/script.js
@@ -2,6 +2,7 @@
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var pageContentEl = document.querySelector("#page-content");
 
 // (1) function 1
 var taskFormHandler = function(event) {
@@ -45,11 +46,12 @@ var createTaskEl = function(taskDataObj) {
   var taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
 
+  // the h3 will pay a role in searching for the task-name later by passing h3.task-name in to querySelector()
   taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
   listItemEl.appendChild(taskInfoEl);
 
   // add button stuff within a task item
-  var taskActionsEl = createTaskActions(taskIdCounter); //new, call to funcion (3)
+  var taskActionsEl = createTaskActions(taskIdCounter); //new, call to funcion (3), returned item is a compact item
   listItemEl.appendChild(taskActionsEl);
   console.log(taskActionsEl);     
 
@@ -101,5 +103,58 @@ var createTaskActions = function(taskId) {
 
   return actionContainerEl;
 };
+
+// (3C)
+var editTask = function(taskId) {
+  //console.log("editing task #" + taskId);
+
+  // get task list item element
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']"); // li 
+  // get content from task name and type
+  var taskName = taskSelected.querySelector("h3.task-name").textContent;
+  var taskType = taskSelected.querySelector("span.task-type").textContent;
+  document.querySelector("input[name='task-name']").value = taskName; // the input box
+  document.querySelector("select[name='task-type']").value = taskType; // the dropdown select
+
+  document.querySelector("#save-task").textContent = "Save Task"; // change content from Add Task to Save Task
+  
+  // To include the task's id:
+  // This will add the taskId to a data-task-id attribute on the form itself. 
+  // It's a new attribute that users won't see but that we can use 
+  // later on to save the correct task (trick)
+  formEl.setAttribute("data-task-id", taskId); // form is the parent item
+
+};
+
+// (3B)
+var deleteTask = function(taskId) {
+  // There's no space between the .task-item and the [data-task-id] attribute, which
+  // means that both properties must be on the same element li that help define element li
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+  taskSelected.remove(); // remove is built-in function ?!
+  //console.log(taskSelected);
+};
+
+// (3A)
+var taskButtonHandler = function(event) {
+  // get target element from event. 
+  // event.target reports the element on which the event occurs, in this case, the click event.
+  // console.log(event.target);
+  var targetEl = event.target;
+  
+  // edit button was clicked
+  if (targetEl.matches(".edit-btn")) {  // matches() doesn't find and return an element. Instead, it returns true if the element would be returned
+    var taskId = targetEl.getAttribute("data-task-id");
+    editTask(taskId);
+  } 
+  else if (targetEl.matches(".delete-btn")){  // matches() doesn't find and return an element. Instead, it returns true if the element would be returned
+    //console.log("you clicked a delete button!");
+    var taskId = event.target.getAttribute("data-task-id");
+    deleteTask(taskId);
+  }
+};
+
+
+pageContentEl.addEventListener("click", taskButtonHandler);
 
 formEl.addEventListener("submit", taskFormHandler);
